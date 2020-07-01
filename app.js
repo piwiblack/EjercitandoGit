d31cb578cd3615c07943302cae8a3a6e7a7fd2e9
Luciano Mazzuca <mazzucaluciano@hotmail.com>
2020-07-01 16:11:00 -0300

    cambios en luciano

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ function sumar (n1,n2){
     return n1 + n2
 }
 console.log(sumar(2+4))
-let cambio = "Luciano";
+let cambio = "Luciano";git 
--- luciano.js
+++ luciano.js
@@ -1,5 +1,6 @@
 let nombre = 'Luciano';
-
 let edad = 20;
+let altura = "1.65";
+let localidad = 'Caseros';
 
 console.log("Hola mundo!");
