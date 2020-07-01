c4d1d9e93bf4fc3482b8473e96bc5d4b8eafd452
piwiblack <leo.chiovaro@gmail.com>
2020-07-01 16:29:30 -0300

    cambio calculadora

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-let calcu = a1 + a2;
+let sumar = n1 + n2;
+let restar = n1 - n2;
+let dividir = n1 / n2;
+let multiplicar = n1 *n2;
 
+function restar (n1, n2){
+    return n1 - n2;
+}
+
+console.log(restar(5-1)
+)
 function sumar (n1,n2){
     return n1 + n2
 }
 console.log(sumar(2+4))
+
+function dividir(n1,n2){
+    return n1 /n2
+}
+console.log(dividir(10/2))
 let cambio = "Luciano";git 
